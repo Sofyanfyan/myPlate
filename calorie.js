48af2721f66d21042bb83e9adc8b5aa990ff085a
melissarsy <melissamey2712@gmail.com>
2022-11-10 23:37:04 +0700

    tambah tabel, fitur add dan delete

--- calorie.js
+++ calorie.js
@@ -40,3 +40,161 @@ let form = {
     weight: 'maintain weight'
 }
 console.log(hitungKalori(form))
+
+const menuList = document.querySelector('.menu-list')
+
+function foodList(){
+  let food = [
+    {
+        nama: 'nasi',
+        calorie: 175,
+        gram: 100
+    },
+    {
+        nama: 'nasi goreng',
+        calorie: 267,
+        gram: 100
+    },
+    {
+        nama: 'sate ayam',
+        calorie: 466,
+        gram: 100
+    },
+    {
+        nama: 'siomay',
+        calorie: 361,
+        gram: 100
+    },
+    {
+        nama: 'telur goreng',
+        calorie: 92,
+        gram: 100
+    },
+    {
+        nama: 'roti tawar',
+        calorie: 128,
+        gram: 50
+    },
+    {
+        nama: 'mie instant',
+        calorie: 168,
+        gram: 50
+    },
+    {
+        nama: 'ayam panggang',
+        calorie: 164.3,
+        gram: 100
+    },
+    {
+        nama: 'pepaya',
+        calorie: 46,
+        gram: 100
+    },
+    {
+        nama: 'apel',
+        calorie: 92,
+        gram: 160
+    },
+    {
+        nama: 'alpukat',
+        calorie: 85,
+        gram: 100
+    },
+    {
+        nama: 'tempe goreng',
+        calorie: 200,
+        gram: 100
+    },
+    {
+        nama: 'pisang ambon',
+        calorie: 74.2,
+        gram: 100
+    },
+    {
+        nama: 'anggur',
+        calorie: 60,
+        gram: 125
+    },
+    {
+        nama: 'sayur lodeh',
+        calorie: 61,
+        gram: 100
+    },
+    {
+        nama: 'sayur asam',
+        calorie: 88,
+        gram: 100
+    },
+    {
+        nama: 'sop bayam',
+        calorie: 78,
+        gram: 50
+    },
+    {
+        nama: 'tahu goreng',
+        calorie: 111,
+        gram: 100
+    },
+ ]
+
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+    let counter = 0
+    // creating all cells
+    for (let i = 0; i < food.length; i++) {
+        
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+        const cell2 = document.createElement("td");
+        const cell3 = document.createElement("td");
+        const cell4 = document.createElement("td");
+        const menuMakanan = document.getElementById('menu-makanan')
+        const cellText = document.createTextNode(`${food[i].nama}`);
+        const gram = document.createTextNode(`${food[i].gram}`);
+        const calorie = document.createTextNode(`${food[i].calorie}`)
+        const infoButton = document.createElement('button')
+        infoButton.innerHTML = 'add'
+        infoButton.classList.add('add-btn')
+        const deleteButton = document.createElement('button')
+        deleteButton.innerHTML = 'add'
+        cell.appendChild(cellText);
+        cell2.appendChild(gram);
+        cell3.appendChild(calorie);
+        row.appendChild(cell);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
+        row.appendChild(infoButton);
+        menuMakanan.appendChild(row);
+        infoButton.addEventListener("click", function(){ 
+        
+        document.getElementById('calorie-harian').innerHTML += `<tr><td>${food[i].nama}</td><td>${food[i].gram}</td><td>${food[i].calorie}</td><td><button onclick="deleteRow(${counter})">Delete</button></td></tr>`; 
+        counter++
+        }); 
+        
+    }
+    
+    // for (let j = 0; j < food.length; j++) {
+    //     const menuMakanan = document.getElementById('calorie-harian')
+    // }
+
+}
+foodList()
+
+function deleteRow(index){
+    let el = document.getElementById('calorie-harian')
+    var children = el.children;
+    if(children.length > 0) {
+        el.removeChild(children[index]);
+    }
+    foodList()
+}
+
+// function deleteWord(){
+//     const menuMakanan = document.getElementById('calorie-harian')
+//     document.getElementById("myTable").deleteRow
+// }
+  
+// function remove(elem){
+//     elem.parentNode.removeChild(elem);
+//   }
+//   <div id="i", onclick="remove(this)">click</div>
